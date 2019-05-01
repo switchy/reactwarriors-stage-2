5f2357f06f59186f480bff33cafd09c162006099
Yuriy Halik <planer.f1a@gmail.com>
2019-05-01 23:50:56 +0300

    first commit

---
+++ components/FormTextField.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const FormTextField = props => {
+  const {
+    id,
+    type,
+    labelText,
+    placeHolder,
+    name,
+    value,
+    error,
+    onChange
+  } = props;
+
+  let optAttrs = {};
+  if (placeHolder) {
+    optAttrs.placeholder = placeHolder
+  }
+
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{labelText}</label>
+      <input
+        type={type}
+        className={"form-control" + (error ? " is-invalid" : "")}
+        name={name}
+        id={id}
+        value={value}
+        onChange={onChange}
+        {...optAttrs}
+      />
+      {error && (
+        <div className="invalid-feedback">
+          {error}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FormTextField;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./stylesheets/index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
